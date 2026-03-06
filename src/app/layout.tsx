f51d8f94bccd938a52a203628134af7b321725d5
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-03-06 15:38:26 +0100

    add new font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import Footer from "./components/footer";
 import "./globals.css";
+import { Baskervville } from "next/font/google";
+
+const baskervville = Baskervville({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Arduino Resources",
@@ -14,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex flex-col">
+      <body className={`${baskervville.className} min-h-screen flex flex-col`}>
         <main className="flex-1 mx-auto w-full max-w-3xl px-4 py-10">
           {children}
         </main>
